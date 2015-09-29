eff4e6681b1a332244bcd37345fdb4ce7708848a
robotto <mark@moore.dk>
2015-09-29 20:58:45 +0200

    changed code on controller from using relays to using a MCP4231 digital potentiometer

--- Player/Player.ino
+++ Player/Player.ino
@@ -29,6 +29,12 @@
 #define BREAKOUT_CS     10     // VS1053 chip select pin (output)
 #define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
 
+//These variables are used for the digital potentiometer:
+const byte REG0=B00000000; //Register 0 Write command
+const byte REG1=B00010000; //Register 1 Write command
+int potCS= 2;
+int i=0;
+
 
 // These are common pins between breakout and shield
 #define CARDCS 4     // Card chip select pin
@@ -49,15 +55,20 @@ Adafruit_VS1053_FilePlayer musicPlayer =
 
 #define NUM_FILES 8
 char *tracks[NUM_FILES] = {"0.mp3", "1.mp3", "2.mp3", "3.mp3", "4.mp3", "5.mp3", "glar.mp3","uffe.mp3"};
-
+/*
 int relayL = 5;
 int relayR = 6;
-
+*/
 void setup() {
+  /*
   pinMode(relayL, OUTPUT);
   pinMode(relayR, OUTPUT);
   digitalWrite(relayL, LOW);
   digitalWrite(relayR, LOW);
+  */
+  pinMode (potCS, OUTPUT);
+  SPI.begin();
+  digitalPotWrite(255); //full on
 
 
   Serial.begin(9600);
@@ -84,22 +95,46 @@ void setup() {
   Serial.println(F("Playing boot"));
   musicPlayer.playFullFile(tracks[0]);
 
-  digitalWrite(relayL, HIGH);
-  digitalWrite(relayR, HIGH);
+        /*for (i = 255; i >= 0; i--) //Crossfade out
+        {
+        digitalPotWrite(i);
+        delay(10);
+        }*/
+        digitalPotWrite(0);
 }
 
 void loop() {
 
   if (Serial.available()) {
-    int i = Serial.parseInt();
-    Serial.println(i);
-    if(i >= 0 && i < NUM_FILES){
+    int integer = Serial.parseInt();
+    Serial.println(integer);
+    if(integer >= 0 && integer < NUM_FILES){
     //  Serial.println("inside if");
-      digitalWrite(relayL, LOW);
-      digitalWrite(relayR, LOW);
-      musicPlayer.playFullFile(tracks[i]);
-      digitalWrite(relayL, HIGH);
-      digitalWrite(relayR, HIGH);
+    
+        /*for (i = 0; i <= 255; i++) //Crossfade in
+        {
+        digitalPotWrite(i);
+        delay(2);
+        }*/
+  digitalPotWrite(255); //full on
+
+    
+//      digitalWrite(relayL, LOW);
+//      digitalWrite(relayR, LOW);
+    
+        musicPlayer.playFullFile(tracks[integer]);
+      
+      /*
+        for (i = 255; i >= 0; i--) //Crossfade out
+        {
+        digitalPotWrite(i);
+        delay(2);
+        }
+      */
+      digitalPotWrite(0);
+      
+//      digitalWrite(relayL, HIGH);
+//      digitalWrite(relayR, HIGH);
       while(Serial.available()){
         Serial.read();
       }
@@ -107,4 +142,19 @@ void loop() {
   }
 
   delay(1);
+}
+
+
+int digitalPotWrite(int value)
+{
+  digitalWrite(potCS, LOW);
+  SPI.transfer(REG0);
+  SPI.transfer(value);
+  digitalWrite(potCS, HIGH);
+  delay(2);
+  digitalWrite(potCS, LOW);
+  SPI.transfer(REG1);
+  SPI.transfer(value);
+  digitalWrite(potCS, HIGH);
+  delay(2);
 }
